f74f47fe5733cba4c99f048ec539a60c5a0d5771
flynn <lognamu@gmail.com>
2019-04-02 19:43:58 +0900

    Updates

---
+++ precache-manifest.1dccf50e5ae0db71cdcbdc2c4dd2c57c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1950e92bbfd0d8ea5452",
+    "url": "/static/css/main.44ec2e72.chunk.css"
+  },
+  {
+    "revision": "1950e92bbfd0d8ea5452",
+    "url": "/static/js/main.6d4e83f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8b669b39e9656397f63",
+    "url": "/static/css/2.a32aa8fe.chunk.css"
+  },
+  {
+    "revision": "d8b669b39e9656397f63",
+    "url": "/static/js/2.2649bcc4.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "cee9cb8f0003e7360652c19cc4e1fab6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0efb883b633941fdfc7a2c019e9f70b9.js"
+  "/precache-manifest.1dccf50e5ae0db71cdcbdc2c4dd2c57c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
